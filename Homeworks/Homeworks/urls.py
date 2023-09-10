fce6302556dfc97053dd695f6ca1b74006af977f
anatoly <tolyan.yalta@yandex.ru>
2023-09-11 02:09:56 +0300

    File Homeworks/Homeworks/urls.py has been modified

--- Homeworks/Homeworks/urls.py
+++ Homeworks/Homeworks/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homework_1_app.urls')),
+    path('', include('homework_2_app.urls')),
 ]
