4b29553a06c46a2e29ee20c6f19717e889838d28
anatoly <tolyan.yalta@yandex.ru>
2023-09-12 16:25:47 +0300

    File Homeworks/homework_1_app/urls.py has been modified

--- Homeworks/homework_1_app/urls.py
+++ Homeworks/homework_1_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    ]
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('about/', views.about, name='about'),
+#     ]
 
 # http://127.0.0.1:8000
 # http://127.0.0.1:8000/about
